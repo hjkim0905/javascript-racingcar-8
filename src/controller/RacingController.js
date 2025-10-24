79652ae676d6d3dbe2f5b23fb4ab09b147e03b5e
hjkim0905 <eax9952@gmail.com>
2025-10-24 18:53:40 +0900

    feat: implement MVC architecture with car name input handling

---
+++ src/controller/RacingController.js
@@ -0,0 +1,20 @@
+import Car from '../model/Car.js';
+import InputHandler from '../view/InputHandler.js';
+import OutputHandler from '../view/OutputHandler.js';
+import { Console } from '@woowacourse/mission-utils';
+
+export default class RacingController {
+  constructor() {
+    this.cars = [];
+    this.inputHandler = new InputHandler();
+    this.outputHandler = new OutputHandler();
+  }
+
+  async run() {
+    const carNamesArray = await this.inputHandler.getCarNames();
+    Console.print(carNamesArray);
+
+    const tryCount = parseInt(await this.inputHandler.getTryCount());
+    Console.print(tryCount);
+  }
+}
---
+++ src/view/OutputHandler.js
@@ -0,0 +1,7 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export default class OutputHandler {
+  printResult(cars) {}
+
+  printWinner(winners) {}
+}
