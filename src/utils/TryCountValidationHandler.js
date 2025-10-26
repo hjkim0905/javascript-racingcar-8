c1223e3941248e8ccdcec0ab38ad22a89ba3008f
hjkim0905 <eax9952@gmail.com>
2025-10-26 13:52:04 +0900

    chore: add 'ERROR' string to the error messages and check if npm test has passed all the test cases

--- src/utils/TryCountValidationHandler.js
+++ src/utils/TryCountValidationHandler.js
@@ -1,19 +1,19 @@
 export default class TryCountValidationHandler {
   isNumber(tryCount) {
     if (Number.isNaN(tryCount)) {
-      throw new Error('숫자가 아닌 값이 입력되었습니다.');
+      throw new Error('[ERROR] 숫자가 아닌 값이 입력되었습니다.');
     }
   }
 
   isInteger(tryCount) {
     if (!Number.isInteger(tryCount)) {
-      throw new Error('정수가 아닌 값이 입력되었습니다.');
+      throw new Error('[ERROR] 정수가 아닌 값이 입력되었습니다.');
     }
   }
 
   isPositiveNumber(tryCount) {
     if (tryCount <= 0) {
-      throw new Error('0 이하인 값이 입력되었습니다.');
+      throw new Error('[ERROR] 0 이하인 값이 입력되었습니다.');
     }
   }
 }
